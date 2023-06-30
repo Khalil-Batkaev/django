500d48de758446c3597e22a25160e5a13db8648f
Khalil Batkaev <xermed@yandex.ru>
2023-06-30 11:35:08 +0300

    [+] вьюшки и контролеры для сайта

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,13 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.hello),
+    path('hello/', views.hello),
+    path('', views.MainPageView.as_view()),
+    path('contacts/', views.ContactsPageView.as_view()),
+    path('courses_list/', views.CoursesPageView.as_view()),
+    path('doc_site/', views.DocPageView.as_view()),
+    path('login/', views.LoginPageView.as_view()),
+    path('news/', views.NewsPageView.as_view()),
 ]
+
+
