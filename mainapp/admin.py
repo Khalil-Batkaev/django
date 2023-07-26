07629e9e6e58fd0298b95056e15c2f43d4124d96
Khalil Batkaev <xermed@yandex.ru>
2023-07-26 13:37:08 +0300

    [+] Добавил админ модели

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from mainapp import models as mainapp_models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "is_deleted"]
+    search_fields = ["title", "description", "body"]
+    actions = ["mark_deleted"]
+    
+    def mark_deleted(self, request, queryset):
+        queryset.update(is_deleted=True)
+
+    mark_deleted.short_description = _("Пометить удаленными")
+
+
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "get_course_name", "lesson_number", "title", "is_deleted"]
+    ordering = ["-course__name", "-lesson_number"]
+    list_per_page = 5
+    list_filter = ["course", "created", "is_deleted"]
+    actions = ["mark_deleted"]
+
+    def get_course_name(self, obj):
+        return obj.course.name
+
+    get_course_name.short_description = _("Course")
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(is_deleted=True)
+
+    mark_deleted.short_description = _("Пометить удаленными")
+
+
+@admin.register(mainapp_models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "is_deleted"]
